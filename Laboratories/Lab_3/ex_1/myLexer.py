5e2db298f78379f90e3153c37e3a4a18494358c8
gaetano_buscema <gaetano.buscema@eng.it>
2020-07-12 18:50:31 +0200

    [Lab_3]: ex_1 bug fixes

--- Laboratories/Lab_3/ex_1/myLexer.py
+++ Laboratories/Lab_3/ex_1/myLexer.py
@@ -18,6 +18,7 @@ class MyLexer():
     # list of TOKENS
     tokens = [
 
+        'ws',
         'RC', 'RO', 'BC', 'BO', 'SO' , 'SC',
         'DIV', 'STAR', 'PLUS', 'MINUS',
         'MIN', 'MAJ', 'MIN_EQ', 'MAJ_EQ', 'EQ_MIN', 'EQ_MAJ', 'EQ',
